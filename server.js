1a692638c61c775f8259722bc7139c5781ce529a
chahthurperera <chathuraperera007@gmail.com>
2022-09-12 21:45:19 +0530

    DB connected

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.static("./public"));
+
+app.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}...`);
+});
